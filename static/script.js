7f1075b4255e11091d48a07160b11d5dc334ce46
aya-aladdin <aya2aladdin@gmail.com>
2026-03-24 16:52:20 +0400

    padding bottom text

--- static/script.js
+++ static/script.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
     inputWrapper.style.borderTop = '1px solid #333';
     inputWrapper.style.zIndex = '1000';
     inputWrapper.style.boxSizing = 'border-box';
-    output.style.paddingBottom = '250px';
+    output.style.paddingBottom = '200px';
     inputLine.style.fontSize = '0.9rem';
 
     hiddenInput.addEventListener('input', () => {
